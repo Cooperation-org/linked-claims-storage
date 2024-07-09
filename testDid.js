f07409f785811a5d52579ce19bb2f1fb6c634619
omar_salah <omar.salah1597@gmail.com>
2024-07-09 23:35:28 +0300

    refactor: Update testDid.js to use GoogleDriveStorage

--- testDid.js
+++ testDid.js
@@ -1,6 +1,8 @@
+import { GoogleDriveStorage } from './dist/index.js';
 import DIDManager from './dist/utils/didManager.js';
 
-const accessToken = 'ADD_YOUR_ACCESS';
+const accessToken =
+	'ya29.a0AXooCgsEs_uLWBlCyz_H8Eeulvw9PE9KztYL82_DTlm-TfD86nZDoOmmAuewpYFPjcrWv3JhyiVNDxzGksixIMnmFmfJKR4XMMrekAePdocxtGQvfn9bk2TyMNJzcYn96uz9rKM2g3K77z2Zzkus5EGJvlKSDMUeenhyaCgYKAS4SARISFQHGX2MiPTFf4LBkyiDZF3p2SGwtkQ0171';
 const didManager = new DIDManager(accessToken);
 
 async function main() {
@@ -34,7 +36,17 @@ async function main() {
 	// console.log('Signed VC:', signedVC);
 
 	// Step 4: Save Signed VC to Google Drive
-	await didManager.saveToGoogleDrive(signedVC, 'VC');
+	await didManager.saveToGoogleDrive(unsignedVC, 'VC');
+
+	// const storage = new GoogleDriveStorage(accessToken);
+	// const parentId = await storage.createFolder('parent');
+	// const childId = await storage.createFolder('child', parentId);
+	// const fileData = {
+	// 	fileName: 'TIMESTAMP.json',
+	// 	mimeType: 'application/json',
+	// 	body: JSON.stringify({ name: 'John Doe' }),
+	// };
+	// const response = await storage.save(fileData, childId);
 }
 
 main().catch(console.error);
