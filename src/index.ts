477cc51f719c31124aa2762bc3e83da950b73121
Omar Salah <omar.salah1597@gmail.com>
2024-12-25 00:39:52 +0200

    feat: refactor Resume class to ResumeStorage and add ResumeVC model for verifiable credentials

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export * from './models/CredentialEngine.js';
 export * from './utils/google.js';
 export * from './utils/presentation.js';
 export * from './models/Resume.js';
+export * from './models/ResumeVC.js';
